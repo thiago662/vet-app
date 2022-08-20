3c96442c255ba0a760ef442f2a1844d8013fcc02
thiago662 <thiago1santos12@gmail.com>
2022-08-20 17:29:04 -0300

    feat: criando tamplate com header e nav
    Resolve: [VET-16]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignComponent } from './sign/sign.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'sign',
+    component: SignComponent,
+  },
+  // {
+  //   path: 'dashboard',
+  //   component: DashboardComponent,
+  // },
+  // {
+  //   path: 'lead',
+  //   component: LeadComponent,
+  // },
+  // {
+  //   path: 'relatorio',
+  //   component: ReportComponent,
+  // },
+  // {
+  //   path: 'lixeira',
+  //   component: TrashComponent,
+  // },
+  {
+    path: 'users',
+    component: UserComponent,
+  },
+  // {
+  //   path: 'profile',
+  //   component: ProfileComponent,
+  // },
+  {
+    path: '**',
+    redirectTo: 'sign',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
